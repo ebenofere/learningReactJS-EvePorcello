adddf9040544871d409a1a4b7d8039f12d336dfb
Ebenezer Ofere <eoofere@sidmach.com>
2022-02-16 13:22:00 +0100

    Conditional Rendering

--- src/index.js
+++ src/index.js
@@ -2,27 +2,54 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const lakeList = [
-  { id: 1, name: "Lake Victoria", country: "Uganda, Tanzania, Kenya" },
-  { id: 2, name: "Lake Malawi", country: "Malawi" },
-  { id: 3, name: "Lake Michigan", country: "United States of America" },
-  { id: 4, name: "Lake Ontario", country: "Canada" },
-  { id: 5, name: "Lake Tahoe", country: "United States of America" },
-  { id: 6, name: "Great Bear Lake", country: "Canada" },
-];
-
-function App({ lakes }) {
-  console.log(lakes);
+function Lake({ name }) {
   return (
     <div>
-      {lakes.map((lake) => (
-        <div key={lake.id}>
-          <h2>{lake.name}</h2>
-          <p>This lake is located in {lake.country}</p>
-        </div>
-      ))}
+      <h1>Visit {name}!</h1>
     </div>
   );
 }
 
-ReactDOM.render(<App lakes={lakeList} />, document.getElementById("root"));
+function SkiResort({ name }) {
+  return (
+    <div>
+      <h1>Visit {name}!</h1>
+    </div>
+  );
+}
+
+// function App(props) {
+//   if (props.season === "summer") {
+//     return <Lake name="Oniru Beach" />;
+//   } else if (props.season === "winter") {
+//     return <SkiResort name="Erin-Ijesha Waterfall" />;
+//   }
+// }
+
+// function App(props) {
+//   return (
+//     <div>
+//       {props.season === "summer" ? (
+//         <Lake name="Oniru Beach" />
+//       ) : (
+//         <SkiResort name="Erin-Ijesha Waterfall" />
+//       )}
+//     </div>
+//   );
+// }
+
+function App(props) {
+  return (
+    <div>
+      {props.season === "summer" ? (
+        <Lake name="Oniru Beach" />
+      ) : props.season === "winter" ? (
+        <SkiResort name="Erin-Ijesha Waterfall" />
+      ) : (
+        <h1>Come back in the winter or summer!</h1>
+      )}
+    </div>
+  );
+}
+
+ReactDOM.render(<App season="summer" />, document.getElementById("root"));
