297f6a5339a7e88b06800cc6628fe9ec7f273e9b
Ebenezer Ofere <eoofere@sidmach.com>
2022-02-16 10:41:42 +0100

    Creating a React Element

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+// ReactDOM.render takes in two arguments. The first argument is the name of the element that we want to create, and the second is where we want to render that element.
+
+// React.createElement takes in three arguments. The first is the name of the tag that we want to create. The second is any properties that we want this element to have. And then the third is any children
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  React.createElement("h1", { style: { color: "red" } }, "Hello!"),
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// When we save this file, we should see "Hello!" being rendered to the browser
+
+// React elements are the building blocks of React applications
