61151758cdd27917fce9340e123ce17b2e43b1ac
Ebenezer Ofere <eoofere@sidmach.com>
2022-02-16 16:32:36 +0100

    Fetching Data with useEffect

--- src/index.js
+++ src/index.js
@@ -2,37 +2,35 @@ import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function App() {
-  const [val, setVal] = useState("");
-  const [val2, setVal2] = useState("");
-
+function GitHubUser({ login }) {
+  const [data, setData] = useState(null);
   useEffect(() => {
-    console.log(`field 1: ${val}`);
-  }, [val]);
+    fetch(`https://api.github.com/users/${login}`)
+      .then((res) => res.json())
+      .then(setData)
+      .catch(console.error);
+  }, []);
 
-  useEffect(() => {
-    console.log(`field 2: ${val2}`);
-  }, [val2]);
-
-  return (
-    <>
-      <label>
-        Favourite Phrase:
-        <input value={val} onChange={(e) => setVal(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Second Favourite Phrase:
-        <input value={val2} onChange={(e) => setVal2(e.target.value)} />
-      </label>
-    </>
-  );
+  if (data) {
+    return (
+      <div>
+        <h1>{data.login}</h1>
+        <img src={data.avatar_url} width="100" />
+      </div>
+    );
+  }
+  return null;
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <GitHubUser login="moontahoe" />,
+  document.getElementById("root")
+);
 
 // "useEffect" allows us to perform side effects inside of our function components
 
 // The things that we want the component to do other than return UI are called "effects". So, an alert, a console log, any sort of interaction with the browser or a native API is not part of the render, it's not part of the "return", so we can use these "useEffect" hooks to do all sorts of cool things.
 
 // The "dependency array" is a really important part of working with React hooks. It's going to allow for smart rendering so we won't trigger unnecessary re-renders if we pass the right values to that array.
+
+// Another way to use the "useEffect" hook is to fetch some data
