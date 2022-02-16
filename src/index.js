911a1ba7a41e49911f46469230c002563ae70d8a
Ebenezer Ofere <eoofere@sidmach.com>
2022-02-16 16:10:08 +0100

    Updating with the useEffect dependency array

--- src/index.js
+++ src/index.js
@@ -2,26 +2,37 @@ import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function Checkbox() {
-  const [checked, setChecked] = useState(false);
+function App() {
+  const [val, setVal] = useState("");
+  const [val2, setVal2] = useState("");
 
   useEffect(() => {
-    alert(`checked: ${checked.toString()}`);
-  });
+    console.log(`field 1: ${val}`);
+  }, [val]);
+
+  useEffect(() => {
+    console.log(`field 2: ${val2}`);
+  }, [val2]);
+
   return (
     <>
-      <input
-        type="checkbox"
-        value={checked}
-        onChange={() => setChecked((checked) => !checked)}
-      />
-      {checked ? "checked" : "not checked"}
+      <label>
+        Favourite Phrase:
+        <input value={val} onChange={(e) => setVal(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Second Favourite Phrase:
+        <input value={val2} onChange={(e) => setVal2(e.target.value)} />
+      </label>
     </>
   );
 }
 
-ReactDOM.render(<Checkbox />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // "useEffect" allows us to perform side effects inside of our function components
 
 // The things that we want the component to do other than return UI are called "effects". So, an alert, a console log, any sort of interaction with the browser or a native API is not part of the render, it's not part of the "return", so we can use these "useEffect" hooks to do all sorts of cool things.
+
+// The "dependency array" is a really important part of working with React hooks. It's going to allow for smart rendering so we won't trigger unnecessary re-renders if we pass the right values to that array.
