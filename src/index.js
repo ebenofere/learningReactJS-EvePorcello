74166029b1215a592fcb576b332aca16ccc27112
Ebenezer Ofere <eoofere@sidmach.com>
2022-02-16 15:30:23 +0100

    Working with useEffect

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function App() {
-  const [year, setYear] = useState(2020);
-  const [manager, setManager] = useState("Alex");
-  const [status, setStatus] = useState("Open");
+function Checkbox() {
+  const [checked, setChecked] = useState(false);
 
+  useEffect(() => {
+    alert(`checked: ${checked.toString()}`);
+  });
   return (
     <>
-      <div>
-        <h1>{year}</h1>
-        <button onClick={() => setYear(year + 1)}>Add +</button>
-      </div>
-
-      <div>
-        <h1>Manager on Duty: {manager}</h1>
-        <button onClick={() => setManager("Rachel")}>Change</button>
-      </div>
+      <input
+        type="checkbox"
+        value={checked}
+        onChange={() => setChecked((checked) => !checked)}
+      />
+      {checked ? "checked" : "not checked"}
     </>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Checkbox />, document.getElementById("root"));
+
+// "useEffect" allows us to perform side effects inside of our function components
+
+// The things that we want the component to do other than return UI are called "effects". So, an alert, a console log, any sort of interaction with the browser or a native API is not part of the render, it's not part of the "return", so we can use these "useEffect" hooks to do all sorts of cool things.
