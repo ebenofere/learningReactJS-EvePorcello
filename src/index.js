07848ccf2bb0c3449e87d32311f28c6a61a1f545
Ebenezer Ofere <eoofere@sidmach.com>
2022-02-16 12:36:21 +0100

    Rendering Lists

--- src/index.js
+++ src/index.js
@@ -2,23 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function Lake({ name }) {
-  return <h1>{name}</h1>;
-}
+const lakeList = [
+  "Lake Victoria",
+  "Lake Malawi",
+  "Lake Michigan",
+  "Lake Ontario",
+  "Lake Tahoe",
+  "Great Bear Lake",
+];
 
-function App() {
+function App({ lakes }) {
+  console.log(lakes);
   return (
-    <div>
-      <Lake name="Lake Victoria" />
-      <Lake name="Lake Malawi" />
-      <Lake name="Lake Michigan" />
-      <Lake name="Lake Ontario" />
-      <Lake name="Lake Tahoe" />
-      <Lake name="Great Bear Lake" />
-    </div>
+    <ul>
+      {lakes.map((a) => (
+        <li>{a}</li>
+      ))}
+    </ul>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
-// Props, or properties, is an object in React that contains properties about the component. With props, we can display dynamic data within a component.
+ReactDOM.render(<App lakes={lakeList} />, document.getElementById("root"));
