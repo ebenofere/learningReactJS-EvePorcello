bae42ab789cbbe740b4af096cf7949bc2d877354
Ebenezer Ofere <eoofere@sidmach.com>
2022-02-16 12:50:16 +0100

    Rendering List of Objects and Adding Keys

--- src/index.js
+++ src/index.js
@@ -3,22 +3,25 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 const lakeList = [
-  "Lake Victoria",
-  "Lake Malawi",
-  "Lake Michigan",
-  "Lake Ontario",
-  "Lake Tahoe",
-  "Great Bear Lake",
+  { id: 1, name: "Lake Victoria", country: "Uganda, Tanzania, Kenya" },
+  { id: 2, name: "Lake Malawi", country: "Malawi" },
+  { id: 3, name: "Lake Michigan", country: "United States of America" },
+  { id: 4, name: "Lake Ontario", country: "Canada" },
+  { id: 5, name: "Lake Tahoe", country: "United States of America" },
+  { id: 6, name: "Great Bear Lake", country: "Canada" },
 ];
 
 function App({ lakes }) {
   console.log(lakes);
   return (
-    <ul>
-      {lakes.map((a) => (
-        <li>{a}</li>
+    <div>
+      {lakes.map((lake) => (
+        <div key={lake.id}>
+          <h2>{lake.name}</h2>
+          <p>This lake is located in {lake.country}</p>
+        </div>
       ))}
-    </ul>
+    </div>
   );
 }
 
